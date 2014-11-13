760180edb892e44b96200b0453e42b49e9ae80cf
static-blur <vjstaticblur@gmail.com>
2014-11-14 11:40:58 +1300

    Create is_blog.php
    Combines all the blog template files into one easy conditional tag "is_blog()"

---
+++ is_blog.php
@@ -0,0 +1,7 @@
+<?php
+function is_blog () {
+	global  $post;
+	$posttype = get_post_type($post );
+	return ( ((is_archive()) || (is_author()) || (is_category()) || (is_home()) || (is_single()) || (is_tag())) && ( $posttype == 'post')  ) ? true : false ;
+}
+?>
